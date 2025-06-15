255ad09e2a83eb45582ca5ccb68c1fa537f55c18
Necxuz18 <u202210938@upc.edu.pe>
2025-06-14 20:20:34 -0500

    feat: integrate ngx-translate for multilingual support across components

--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.css'
 })
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
